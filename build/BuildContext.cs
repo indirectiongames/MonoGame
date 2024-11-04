0be540a38feae6d2fa633419e115ee326c7ff61a
Robert Kaufmann <robert-kaufmann@live.com>
2024-11-04 16:39:15 +0100

    Make custom version
    This marks the start of custom commits for the inDirection Games AB
    repository of MonoGame.

--- build/BuildContext.cs
+++ build/BuildContext.cs
@@ -21,9 +21,9 @@ public enum ProjectType
 
 public class BuildContext : FrostingContext
 {
-    public static string VersionBase = "3.8.4";
+    public static string VersionBase = "10.0.0";
     public static readonly Regex VersionRegex = new(@"^v\d+.\d+.\d+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-    public static readonly string DefaultRepositoryUrl = "https://github.com/MonoGame/MonoGame";
+    public static readonly string DefaultRepositoryUrl = "https://github.com/indirectiongames/MonoGame";
 
     public BuildContext(ICakeContext context) : base(context)
     {
